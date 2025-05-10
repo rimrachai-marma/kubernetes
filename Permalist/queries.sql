aa7f3cb8b46e7817b813db4ab1cd0d3783861ea5
rimrachai-marma <rimrachai02@gmail.com>
2025-05-10 17:13:54 +0600

    After practice ingress

--- Permalist/queries.sql
+++ Permalist/queries.sql
@@ -4,16 +4,6 @@ CREATE TABLE items (
 );
 
 INSERT INTO items (title) VALUES ('Buy milk'), ('Finish homework');
--- docker exec -it postgresdb sh
--- kubectl exec -it postgres-deployment-764f8b7bfd-gwxwf -- sh
-
--- psql PGPASSWORD=<password> psql -U <postgres-user> -d <database-name>
-
--- psql -U postgres -d permalist
-
-kubectl apply -f postgres-pv.yml
-kubectl apply -f postgres-pvc.yml
-kubectl apply -f postgres-config.yml
-kubectl apply -f postgres-secret.yml
-kubectl apply -f postgres-db.yml
-kubectl apply -f permalist-app.yml
+INSERT INTO items (title) VALUES ('Go for a walk'), ('Read a book');
+INSERT INTO items (title) VALUES ('Watch a movie'), ('Cook dinner');
+INSERT INTO items (title) VALUES ('Clean the house'), ('Call a friend');
